4327ff5a0265b04adf55b25b4f7f96126d538408
Josh <jalexy12@gmail.com>
2017-02-01 11:31:54 -0500

    Starter Code

---
+++ app.js
@@ -0,0 +1,41 @@
+const express        = require('express');
+const path           = require('path');
+const expressLayouts = require('express-ejs-layouts');
+const favicon        = require('serve-favicon');
+const logger         = require('morgan');
+const cookieParser   = require('cookie-parser');
+const bodyParser     = require('body-parser');
+const mongoose       = require('mongoose');
+
+mongoose.connect('mongodb://localhost/linkedin-clone-development');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.set('layout', 'layouts/main-layout');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const index = require('./routes/index');
+app.use('/', index);
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
